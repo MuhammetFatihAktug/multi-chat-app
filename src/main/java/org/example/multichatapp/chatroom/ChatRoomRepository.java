9fd28ce225b529ddfc3a165ab5450a55b2594e80
Muhammet Fatih Aktug <muhammetfatihaktug@gmail.com>
2024-03-27 00:25:30 +0300

    chatroom mechanism added

---
+++ src/main/java/org/example/multichatapp/chatroom/ChatRoomRepository.java
@@ -0,0 +1,9 @@
+package org.example.multichatapp.chatroom;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface ChatRoomRepository extends MongoRepository<ChatRoom, String> {
+    Optional<ChatRoom> findBySenderIdAndRecipientId(String senderId, String recipientId);
+}
---
+++ src/main/java/org/example/multichatapp/chatroom/ChatRoomService.java
@@ -0,0 +1,43 @@
+package org.example.multichatapp.chatroom;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ChatRoomService {
+
+    private final ChatRoomRepository repository;
+
+    public Optional<String> getChatRoomId(String senderId, String recipientId, boolean createNewRoomIfNotExists) {
+        return repository.findBySenderIdAndRecipientId(senderId, recipientId).map(ChatRoom::getChatId).or(() -> {
+            if (createNewRoomIfNotExists) {
+                var chatId = createChatId(senderId, recipientId);
+                return Optional.of(chatId);
+            }
+            return Optional.empty();
+        });
+    }
+
+    private String createChatId(String senderId, String recipientId) {
+
+        var chatId = String.format("%s_%s", senderId, recipientId);
+        ChatRoom senderRecipient = ChatRoom
+                .builder().chatId(chatId)
+                .senderId(senderId)
+                .recipientId(recipientId)
+                .build();
+
+        ChatRoom recipientSender = ChatRoom
+                .builder()
+                .chatId(chatId)
+                .senderId(recipientId)
+                .recipientId(senderId)
+                .build();
+        repository.save(senderRecipient);
+        repository.save(recipientSender);
+        return chatId;
+    }
+}
