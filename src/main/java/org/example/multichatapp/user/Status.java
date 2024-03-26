2d2fe2dcb2225cbef6d891ffb707a9f2b0673f8f
Muhammet Fatih Aktug <muhammetfatihaktug@gmail.com>
2024-03-26 23:17:42 +0300

    user module added

---
+++ src/main/java/org/example/multichatapp/user/Status.java
@@ -0,0 +1,6 @@
+package org.example.multichatapp.user;
+
+public enum Status {
+
+    ONLINE, OFFLINE
+}
---
+++ src/main/java/org/example/multichatapp/user/UserRepository.java
@@ -0,0 +1,9 @@
+package org.example.multichatapp.user;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    List<User> findAllByStatus(Status status);
+}
