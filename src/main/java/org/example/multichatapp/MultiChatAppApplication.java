50226057f87e792fe99fed76ac7847f6f6b873b9
Muhammet Fatih Aktug <muhammetfatihaktug@gmail.com>
2024-03-26 21:23:51 +0300

    Initial commit

---
+++ src/main/java/org/example/multichatapp/MultiChatAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.multichatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiChatAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/multichatapp/MultiChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.multichatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiChatAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
